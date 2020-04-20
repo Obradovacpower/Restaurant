c6e2b248e42e937270c04bfe96e2ecc5cb376533
Ermin Obradovac <au592117@uni.au.dk>
2020-04-20 13:41:59 +0200

    add project

---
+++ Morgenmadsbuffet/Morgenmadsbuffet/Models/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Morgenmadsbuffet.Models
+{
+    public class Worker
+    {
+        [Key]
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public int role { get; set; }
+    }
+}
