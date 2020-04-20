41b2cd6996bf10be79fe357b7f23179ca1dbd836
Ermin Obradovac <au592117@uni.au.dk>
2020-04-20 14:03:33 +0200

    Added identity, mangler claims

---
+++ MorgenBuffet/MorgenBuffet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MorgenBuffet.Areas.Identity.Data;
+using MorgenBuffet.Data;
+
+[assembly: HostingStartup(typeof(MorgenBuffet.Areas.Identity.IdentityHostingStartup))]
+namespace MorgenBuffet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MorgenBuffetContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MorgenBuffetContextConnection")));
+
+                services.AddDefaultIdentity<MorgenBuffetUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MorgenBuffetContext>();
+            });
+        }
+    }
+}
